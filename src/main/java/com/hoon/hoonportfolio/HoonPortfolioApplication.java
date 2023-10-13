2bacdee75d8ede4bccfe2906088f74af2c7565fd
hoony00 <nasi3611@naver.com>
2023-10-13 17:10:16 +0900

    포토폴리오 웹사이트

---
+++ src/main/java/com/hoon/hoonportfolio/HoonPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.hoon.hoonportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoonPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoonPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoon/hoonportfolio/HoonPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoon.hoonportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoonPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
